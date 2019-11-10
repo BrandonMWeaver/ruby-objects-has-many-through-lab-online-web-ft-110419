234f637b34562f8e6d3348b25ae2a93d6f8d6432
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 16:44:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,26 @@
 class Patient
+  attr_accessor :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def appointments
+    return Appointment.all.select { |appointment| appointment.patient == self }
+  end
+  
+  def new_appointment(doctor, date)
+    return appointments.new(name, self, genre)
+  end
+  
+  def patients
+    return appointments.map { |appointment| appointment.patient }
+  end
 end
